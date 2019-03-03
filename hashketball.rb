b351ea221fb705483069c3f2285a2fb8ac0c9aed
Robert Tate <btate712@gmail.com>
2019-03-03 18:33:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,6 @@ end
 
 def team_colors(team_name)
   game_hash.each do |team, team_data|
-    binding.pry
     if team_data[:team_name] == team_name
       return_info = team_data[:colors]
     end
