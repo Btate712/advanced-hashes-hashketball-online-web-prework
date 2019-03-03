4dfa3f473079697590190fee54aa3a677b1e9764
Robert Tate <btate712@gmail.com>
2019-03-03 18:26:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,3 +114,13 @@ def num_points_scored(player)
     end
   end
 end
+
+def shoe_size(player)
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |name, stats|
+      if name == player
+        return stats[:shoe]
+      end
+    end
+  end
+end
