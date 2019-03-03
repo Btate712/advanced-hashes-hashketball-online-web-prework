5068fba519ceb9e781282297aaa7b9d67893e4f2
Robert Tate <btate712@gmail.com>
2019-03-03 18:24:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -108,6 +108,7 @@ end
 def num_points_scored(player)
   game_hash.each do |team, team_data|
     team_data[:players].each do |name, stats|
+      binding.pry 
       if name == player
         return stats[:points]
       end
